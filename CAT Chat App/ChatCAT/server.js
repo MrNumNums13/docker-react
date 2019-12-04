4f7b7d4ebb4a34470dbdd81702fe3d331d8f6623
Harry Hanskat <harryhanskat13@gmail.com>
2019-12-03 20:53:32 -0700

    Express Router

--- CAT Chat App/ChatCAT/server.js
+++ CAT Chat App/ChatCAT/server.js
@@ -1,18 +1,15 @@
-'use strit';
+'use strict';
 //These first two constants start up the application as an express app.
 const express = require('express');
 const app = express();
+const chatCat = require('./app');
+
 //This line sets the app to make sure it's always on port 3000 so we don't have to set it manually
 app.set('port', process.env.PORT || 3000);
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-//This first get function defines what will happen on the main page when you go to localhost:3000
-app.get('/', (req, res, next) => {
-	//res.send('<h1>Hello Express!</h1>');
-	res.render('login', {
-		pageTitle: 'My Login Page'
-	});
-});
+app.use('/', chatCat.router);
 
 //This is the function that starts the webserver on the specified port
 app.listen(app.get('port'), () => {
